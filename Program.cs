a8a5b324b243526f20c0671ab44c2562de1a6474
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-07-19 14:36:42 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "DataPlatform Deployment in Production");
+app.MapGet("/", () => "DataPlatform Deployment in main");
 
 app.Run();
