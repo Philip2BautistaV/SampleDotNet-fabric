e44c6282b1b05cb00ddf6db1bf4171551c191372
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-07-19 15:06:24 +0800

    Update response message in root endpoint

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "DataPlatform Deployment in main");
+app.MapGet("/", () => "DataPlatform in main");
 
 app.Run();
